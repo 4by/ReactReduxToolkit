30d323a91c70eaef4ecdae8a035ec026c7ba77ec
Victor Ciornii <ciorniivictor96@gmail.com>
2021-09-09 12:12:53 +0300

    createSlice

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   increment,
   decrement,
-} from './toolkitRedux/toolkitReducer';
+  addTodo,
+  removeLastTodo,
+} from './toolkitRedux/toolkitSlice';
+
+const addAsyncTodo = () => {
+  return async (dispatch) => {
+    setTimeout(() => {
+      dispatch(addTodo('ASYNC TODO'));
+    }, 2000);
+  };
+};
 
 const App = () => {
   const count = useSelector((state) => state.toolkit.count);
-  //const todos = useSelector((state) => state.main.todos);
+  const todos = useSelector((state) => state.toolkit.todos);
   const dispatch = useDispatch();
 
   return (
@@ -15,15 +25,16 @@ const App = () => {
       <h1>Counter {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
-      {/* <button onClick={() => dispatch(removeLastTodo())}>
+      <button onClick={() => dispatch(removeLastTodo())}>
         Remove last Todo
       </button>
       <button onClick={() => dispatch(addTodo(prompt()))}>Add Todo</button>
+      <button onClick={() => dispatch(addAsyncTodo())}>Add Async Todo</button>
       <ul>
         {todos.map((todo, idx) => (
           <li key={idx}>{todo}</li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 };
